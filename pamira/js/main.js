590deed3e2b81073913a91c9a2f8f4d671a3ae3a
Osster <_oster_@mail.ru>
2018-05-25 04:44:18 +0300

    Pamira Slider Upd

--- pamira/js/main.js
+++ pamira/js/main.js
@@ -16223,6 +16223,35 @@ var DateFormatter;!function(){"use strict";var e,t,a,r,n,o,i;o=864e5,i=3600,e=fu
 
 })));
 
+var topSlider = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+
+    loop: true,
+
+    roundLengths: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+topSlider.on('slideChange', function (e) {
+    var slider = this;
+    var style = $(slider.slides[slider.activeIndex]).data('style');
+    if (style == 'light') {
+        $('header').removeClass('light').addClass('dark');
+    } else {
+        $('header').removeClass('dark').addClass('light');
+    }
+    //console.log('slideChange style', style);
+});
 
 $(document).ready(function () {
 
