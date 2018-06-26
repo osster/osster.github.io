754bf2274c4af818277b981896d2980ca0896400
Osster <_oster_@mail.ru>
2018-06-27 01:12:36 +0300

    PAMIRA HOME DESKCTOP ALFA

--- pamira/js/main.js
+++ pamira/js/main.js
@@ -16223,7 +16223,7 @@ var DateFormatter;!function(){"use strict";var e,t,a,r,n,o,i;o=864e5,i=3600,e=fu
 
 })));
 
-var topSlider = new Swiper ('.swiper-container', {
+var topSlider = new Swiper ('.top-slider .swiper-container', {
     // Optional parameters
     direction: 'horizontal',
 
@@ -16233,15 +16233,16 @@ var topSlider = new Swiper ('.swiper-container', {
 
     // If we need pagination
     pagination: {
-        el: '.swiper-pagination',
+        el: '.top-slider .swiper-pagination',
     },
 
     // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.top-slider .swiper-button-next',
+        prevEl: '.top-slider .swiper-button-prev',
     },
 });
+
 topSlider.on('slideChange', function (e) {
     var slider = this;
     var style = $(slider.slides[slider.activeIndex]).data('style');
@@ -16252,6 +16253,102 @@ topSlider.on('slideChange', function (e) {
     }
     //console.log('slideChange style', style);
 });
+
+
+var brandSlider = new Swiper ('.wr-brands .swiper-container', {
+    // Optional parameters
+
+    slidesPerView: 6,
+    paceBetween: 30,
+    direction: 'horizontal',
+    loop: true,
+    //roundLengths: true,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.wr-brands .swiper-button-next',
+        prevEl: '.wr-brands .swiper-button-prev'
+    }
+});
+
+
+var eventsSlider = new Swiper ('.wr-events .swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+
+    loop: true,
+
+    roundLengths: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.wr-events .swiper-button-next',
+        prevEl: '.wr-events .swiper-button-prev'
+    }
+});
+
+
+var eventsMonthSlider = new Swiper ('.wr-events-month .swiper-container', {
+    // Optional parameters
+
+    slidesPerView: 4,
+    direction: 'horizontal',
+    loop: true,
+    roundLengths: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.wr-events-month .swiper-button-next',
+        prevEl: '.wr-events-month .swiper-button-prev'
+    }
+});
+
+
+var narrowSlider = new Swiper ('.wr-slider-narrow .swiper-container', {
+    // Optional parameters
+
+    direction: 'horizontal',
+    loop: true,
+    roundLengths: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.wr-slider-narrow .swiper-button-next',
+        prevEl: '.wr-slider-narrow .swiper-button-prev'
+    }
+});
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [47.249076, 39.729340],
+            zoom: 13,
+            controls: ['zoomControl']
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark01 = new ymaps.Placemark([47.228534, 39.71362], {
+            balloonContentHeader: "Красноармейская 63/90",
+            balloonContentBody: "Розничный отдел:<br/> +7 (863) 302-03-04<br/> +7 (919) 888-6-777 <br/> Время\n" +
+            "работы:<br/> ПН-СБ 10:00-19:00<br/> ВС 10:00-16:00<br/> td@pamira.ru<br/>",
+            hintContent: "Красноармейская 63/90"
+        });
+    myPlacemark02 = new ymaps.Placemark([47.267786, 39.761483], {
+        balloonContentHeader: "Троллейбусная 24/2В",
+        balloonContentBody: "Офис:<br/> +7 (863) 300-52-97<br/> Время работы:<br/> ПН-ПТ 10:00-18:00<br/>",
+        hintContent: "Троллейбусная 24/2В"
+    });
+    myPlacemark03 = new ymaps.Placemark([47.269126, 39.761824], {
+        balloonContentHeader: "50-летия Ростсельмаша, 2-6/22Б",
+        balloonContentBody: "Склад:<br/> +7 (863) 219-21-39<br/> Время работы:<br/> ПН-ПТ 9:00-18:00",
+        hintContent: "50-летия Ростсельмаша, 2-6/22Б"
+    });
+
+    myMap.behaviors.disable(['scrollZoom'])
+
+    myMap.geoObjects.add(myPlacemark01);
+    myMap.geoObjects.add(myPlacemark02);
+    myMap.geoObjects.add(myPlacemark03);
+}
 
 $(document).ready(function () {
 
