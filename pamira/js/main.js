1b6d3d47e6c2af6af1ad8b7eb52c2720b0762c11
Osster <_oster_@mail.ru>
2018-06-29 14:02:19 +0300

    PAMIRA UPD

--- pamira/js/main.js
+++ pamira/js/main.js
@@ -16258,18 +16258,34 @@ topSlider.on('slideChange', function (e) {
 var brandSlider = new Swiper ('.wr-brands .swiper-container', {
     // Optional parameters
 
-    slidesPerView: 6,
-    paceBetween: 30,
+    slidesPerView: 4,
+    //paceBetween: 30,
     direction: 'horizontal',
     loop: true,
-    //roundLengths: true,
+    roundLengths: true,
+
     // Navigation arrows
     navigation: {
         nextEl: '.wr-brands .swiper-button-next',
         prevEl: '.wr-brands .swiper-button-prev'
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is <= 320px
+        540: {
+            slidesPerView: 1
+        },
+        // when window width is <= 480px
+        720: {
+            slidesPerView: 2
+        },
+        // when window width is <= 640px
+        960: {
+            slidesPerView: 3
+        }
     }
 });
-
 
 var eventsSlider = new Swiper ('.wr-events .swiper-container', {
     // Optional parameters
@@ -16299,6 +16315,22 @@ var eventsMonthSlider = new Swiper ('.wr-events-month .swiper-container', {
     navigation: {
         nextEl: '.wr-events-month .swiper-button-next',
         prevEl: '.wr-events-month .swiper-button-prev'
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is <= 320px
+        540: {
+            slidesPerView: 1
+        },
+        // when window width is <= 480px
+        720: {
+            slidesPerView: 2
+        },
+        // when window width is <= 640px
+        1200: {
+            slidesPerView: 3
+        }
     }
 });
 
@@ -16349,6 +16381,42 @@ function init() {
     myMap.geoObjects.add(myPlacemark02);
     myMap.geoObjects.add(myPlacemark03);
 }
+
+//Map на странице КОНТАКТЫ
+
+// ymaps.ready(init);
+//
+// function init() {
+//     var myMapContacts = new ymaps.Map("map", {
+//             center: [47.249076, 39.729340],
+//             zoom: 14,
+//             controls: ['zoomControl']
+//         }, {
+//             searchControlProvider: 'yandex#search'
+//         }),
+//         myPlacemark01 = new ymaps.Placemark([47.228534, 39.71362], {
+//             balloonContentHeader: "Красноармейская 63/90",
+//             balloonContentBody: "Розничный отдел:<br/> +7 (863) 302-03-04<br/> +7 (919) 888-6-777 <br/> Время\n" +
+//             "работы:<br/> ПН-СБ 10:00-19:00<br/> ВС 10:00-16:00<br/> td@pamira.ru<br/>",
+//             hintContent: "Красноармейская 63/90"
+//         });
+//     myPlacemark02 = new ymaps.Placemark([47.267786, 39.761483], {
+//         balloonContentHeader: "Троллейбусная 24/2В",
+//         balloonContentBody: "Офис:<br/> +7 (863) 300-52-97<br/> Время работы:<br/> ПН-ПТ 10:00-18:00<br/>",
+//         hintContent: "Троллейбусная 24/2В"
+//     });
+//     myPlacemark03 = new ymaps.Placemark([47.269126, 39.761824], {
+//         balloonContentHeader: "50-летия Ростсельмаша, 2-6/22Б",
+//         balloonContentBody: "Склад:<br/> +7 (863) 219-21-39<br/> Время работы:<br/> ПН-ПТ 9:00-18:00",
+//         hintContent: "50-летия Ростсельмаша, 2-6/22Б"
+//     });
+//
+//     myMap.behaviors.disable(['scrollZoom'])
+//
+//     myMap.geoObjects.add(myPlacemark01);
+//     myMap.geoObjects.add(myPlacemark02);
+//     myMap.geoObjects.add(myPlacemark03);
+// }
 
 $(document).ready(function () {
 
