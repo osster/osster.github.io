c1445e1a16bb94ed0ca96d54681df3dfbb81b789
Osster <_oster_@mail.ru>
2018-07-05 21:07:21 +0300

    PAMIRA CATALOG INNER UPD

--- pamira/js/main.js
+++ pamira/js/main.js
@@ -16341,19 +16341,19 @@ var eventsMonthSlider = new Swiper ('.wr-events-month .swiper-container', {
 });
 
 
-var narrowSlider = new Swiper ('.wr-slider-narrow .swiper-container', {
-    // Optional parameters
-
-    direction: 'horizontal',
-    loop: true,
-    roundLengths: true,
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.wr-slider-narrow .swiper-button-next',
-        prevEl: '.wr-slider-narrow .swiper-button-prev'
-    }
-});
+// var narrowSlider = new Swiper ('.wr-slider-narrow .swiper-container', {
+//     // Optional parameters
+//
+//     direction: 'horizontal',
+//     loop: true,
+//     roundLengths: true,
+//
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.wr-slider-narrow .swiper-button-next',
+//         prevEl: '.wr-slider-narrow .swiper-button-prev'
+//     }
+// });
 
 
 // Переключение событий по месяцам в слайдере "eventsMonthSlider"
@@ -16379,6 +16379,63 @@ $eventsMonth.each(function () {
 
     });
 
+});
+$(document).ready(function () {
+
+    var narrowSlider = $('.wr-slider-narrow');
+
+    $.each(narrowSlider, function () {
+        var narrowSliderID = $(this).attr('id');
+        var narrowSliderDiv = $('#' + narrowSliderID).find('.swiper-container');
+
+        var narrowSliderNext = $('#' + narrowSliderID).find('.swiper-button-next');
+
+        var narrowSliderPrev = $('#' + narrowSliderID).find('.swiper-button-prev');
+
+        var narrowSliderPag = $('#' + narrowSliderID).find('.swiper-pagination');
+
+
+        var narrowSliderSwiper = new Swiper(narrowSliderDiv, {
+            loop: true,
+            navigation: {
+                nextEl: narrowSliderNext,
+                prevEl: narrowSliderPrev
+            },
+            pagination: {
+                el: narrowSliderPag,
+                clickable: true
+            }
+        });
+    });
+});
+$(document).ready(function () {
+
+
+    var sliderText = $('.wr-slider-text');
+
+    $.each(sliderText, function () {
+        var sliderTextID = $(this).attr('id');
+        var sliderTextDiv = $('#' + sliderTextID).find('.swiper-container');
+
+        var sliderTextNext = $('#' + sliderTextID).find('.swiper-button-next');
+
+        var sliderTextPrev = $('#' + sliderTextID).find('.swiper-button-prev');
+
+        var sliderTextPag = $('#' + sliderTextID).find('.swiper-pagination');
+
+
+        var sliderTextSwiper = new Swiper(sliderTextDiv, {
+            loop: true,
+            navigation: {
+                nextEl: sliderTextNext,
+                prevEl: sliderTextPrev
+            },
+            pagination: {
+                el: sliderTextPag,
+                clickable: true
+            }
+        });
+    });
 });
 $(document).ready(function () {
     var catalogBrandSlider = new Swiper('.catalog-brand-slider .swiper-container', {
@@ -18958,6 +19015,88 @@ $(document).ready(function () {
 //         return false;
 //     });
 // });
+$(document).ready(function () {
+
+    // var $icon = $('.wr-flying-nav_links_icon');
+    // var $icon = $('.wr-flying-nav_links_icon [data-index="0"]');
+    //
+    // var $iconPage = $('.wr-icon');
+    // var $iconPageIndex = $iconPage.attr('data-index');
+    //
+    // $icon.each(function () {
+    //     var $this = $(this);
+    //     var $iconIndex = $this.attr('data-index');
+    //
+    //     $this. on('click', function () {
+    //         //$iconPage.removeClass('icon-active');
+    //         $iconPage.attr('data-index','0').toggleClass('icon-active');
+    //
+    //
+    //     });
+    //
+    // });
+
+    var $icons = $('.wr-flying-nav_links_icon');
+    var $flyingPages = $('.wr-icon');
+
+
+    var $iconSearch = $('#icon-search');
+    var $iconCall = $('#icon-call');
+    var $iconBasket = $('#icon-basket');
+    var $iconSelected = $('#icon-selected');
+
+    var $search = $('#search');
+    var $call = $('#call');
+    var $basket = $('#basket');
+    var $selected = $('#selected');
+
+    $iconSearch.on('click', function () {
+
+        if($search.hasClass('icon-active')){
+            $search.removeClass('icon-active');
+        }
+        else {
+            $flyingPages.removeClass('icon-active');
+            $search.toggleClass('icon-active');
+        }
+        //alert("Ok");
+        return false
+    });
+
+    $iconCall.on('click', function () {
+        if($call.hasClass('icon-active')){
+            $call.removeClass('icon-active');
+        }
+        else {
+            $flyingPages.removeClass('icon-active');
+            $call.toggleClass('icon-active');
+        }
+        return false
+    });
+
+    $iconBasket.on('click', function () {
+        if($basket.hasClass('icon-active')){
+            $basket.removeClass('icon-active');
+        }
+        else {
+            $flyingPages.removeClass('icon-active');
+            $basket.toggleClass('icon-active');
+        }
+        return false
+    });
+
+    $iconSelected.on('click', function () {
+        if($selected.hasClass('icon-active')){
+            $selected.removeClass('icon-active')
+        }
+        else {
+            $flyingPages.removeClass('icon-active');
+            $selected.toggleClass('icon-active');
+        }
+        return false
+    });
+
+});
 $(document).ready(function () {
     // var singleSwiper = new Swiper('.single-item_imgs .swiper-container', {
     //     direction: 'vertical',
