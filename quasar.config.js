bbc439b2a8892c0644451d0d79712415349b88b4
alex.osster <alex.osster.web@gmail.com>
2024-01-19 21:53:24 +0100

    Git fix

--- quasar.config.js
+++ quasar.config.js
@@ -87,7 +87,7 @@ module.exports = configure((ctx) => ({
 
         // if you want to use named tokens in your Vue I18n messages, such as 'Hello {name}',
         // you need to set `runtimeOnly: false`
-        // runtimeOnly: false,
+        runtimeOnly: ctx.dev,
 
         // you need to set i18n resource including paths !
         include: path.resolve(__dirname, './src/i18n/**'),
